79668c7ed1caa74307a242eb7979b7353eda8379
Zfjosma97 <49376541+Zfjosma97@users.noreply.github.com>
2019-05-19 13:22:40 -0500

    Nuevo movimiento del player, Fucionamiento de la bomba y nuevos prefabs (19/05/2019)
    El nuevo prefab del player, con el movimiento chingón se llama "PlayerOptimized", de ahora arrastrar ese a las escenas donde se quiera probar el juego. Quitar de las escnas el prefab del player anterior.

---
+++ Assets/SkyScavanger/Scripts/Tools/Bomb/Bomb_Prefab.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb_Prefab : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "R_O")
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                other.gameObject.SetActive(false);
+            }
+           }
+        }
+    }
+
